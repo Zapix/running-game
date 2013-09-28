47e99d094e94aa8dd169bf63af9c106798de0380
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2013-09-29 01:41:19 +0600

    server config

--- src/runninggame/runninggame/settings.py
+++ src/runninggame/runninggame/settings.py
@@ -44,7 +44,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = get_abspath(PROJECT_ROOT, 'media')
+MEDIA_ROOT = get_abspath(PROJECT_ROOT, os.path.pardir, os.path.pardir, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -55,7 +55,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = get_abspath(PROJECT_ROOT, 'static')
+STATIC_ROOT = get_abspath(PROJECT_ROOT, os.path.pardir, os.path.pardir, 'st')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
