756788f351984a3da1e64da394222ed287681605
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2013-09-29 14:59:24 +0600

    Add home and about pages

--- src/runninggame/runninggame/urls.py
+++ src/runninggame/runninggame/urls.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from django.conf import settings
+from django.views import generic as cbv
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,9 +9,15 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('simplegame.urls')),
-    url(r'^', include('websocket.urls')),
+    url(r'^$', cbv.TemplateView.as_view(template_name='home.html'),
+        name='home'),
+    url(r'^about/$', cbv.TemplateView.as_view(template_name='about.html'),
+        name='about'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^', include('websocket.urls')),
+
+    url(r'^game/', include('simplegame.urls')),
 
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^accounts/', include('userprofile.urls'))
