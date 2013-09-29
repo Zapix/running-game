d84e8a4d9a6808bd2727e52ac02625542e6fb83f
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2013-09-29 15:12:19 +0600

    Add game model

--- src/runninggame/simplegame/models.py
+++ src/runninggame/simplegame/models.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, related_name='game_set', null=True,
+                             blank=True)
+    score = models.IntegerField('Score')
+    created = models.DateTimeField('Created', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Played game'
+        verbose_name_plural = 'Played games'
+        ordering = ['-score', ]
+
+    def __unicode__(self):
+        if self.id:
+            return u'%s: %s' % (
+                (self.user or u'Anonymous'),
+                unicode(self.created)
+            )
+        else:
+            return u'new game'
